f4050bc47cd38a465457e5dbec8931f19c6eb15c
chitrasaindane <chitrasaindane@gmail.com>
2025-02-24 20:38:01 +0530

    PostJsonRequest

--- 20_webreqverbs/main.go
+++ 20_webreqverbs/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	fmt.Println("Welcome to web verb")
 	PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
 func PerformGetRequest() {
@@ -36,4 +37,29 @@ func PerformGetRequest() {
 
 	//fmt.Println(content)
 	//fmt.Println(string(content))
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	//fake json payload
+
+	requestBody := strings.NewReader(`
+		{
+			"coursename":"Let's go with golang",
+			"price": 0,
+			"platform":"learnCodeOnline.in"
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
 }
